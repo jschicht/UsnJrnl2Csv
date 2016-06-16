41e235ed988aa34fdabb79a1e61926c840a70cba
Joakim Schicht <joakim.schicht@gmail.com>
2016-06-17 00:32:59 +0200

    Improved db schema for usnjrnl table.

--- UsnJrnl-schema.sql
+++ UsnJrnl-schema.sql
@@ -1,17 +1,19 @@
 
 CREATE TABLE usnjrnl(
-   `Offset`                  VARCHAR(18) NOT NULL
-  ,`FileName`                VARCHAR(255) NOT NULL
-  ,`USN`                     BIGINT  NOT NULL
-  ,`Timestamp`               DATETIME(6)  NOT NULL
-  ,`Reason`                  VARCHAR(255) NOT NULL
-  ,`MFTReference`            BIGINT  NOT NULL
-  ,`MFTReferenceSeqNo`       INTEGER  NOT NULL
-  ,`MFTParentReference`      BIGINT  NOT NULL
-  ,`MFTParentReferenceSeqNo` INTEGER  NOT NULL
-  ,`FileAttributes`          VARCHAR(64) NOT NULL
-  ,`MajorVersion`            INTEGER  NOT NULL
-  ,`MinorVersion`            INTEGER  NOT NULL
-  ,`SourceInfo`              VARCHAR(10)  NOT NULL
-  ,`SecurityId`              INTEGER  NOT NULL
+	`Id`						INT(11) NOT NULL AUTO_INCREMENT
+	,`Offset`                  	VARCHAR(18) NOT NULL
+	,`FileName`                	VARCHAR(255) NOT NULL
+	,`USN`                     	BIGINT  NOT NULL
+	,`Timestamp`               	DATETIME(6)  NOT NULL
+	,`Reason`                  	VARCHAR(255) NOT NULL
+	,`MFTReference`            	BIGINT  NOT NULL
+	,`MFTReferenceSeqNo`       	INT(5)  NOT NULL
+	,`MFTParentReference`      	BIGINT  NOT NULL
+	,`MFTParentReferenceSeqNo` 	INT(5)  NOT NULL
+	,`FileAttributes`          	VARCHAR(64) NOT NULL
+	,`MajorVersion`            	INT(5)  NOT NULL
+	,`MinorVersion`            	INT(5)  NOT NULL
+	,`SourceInfo`              	VARCHAR(10)  NOT NULL
+	,`SecurityId`              	INT(11)  NOT NULL
+	,PRIMARY KEY (Id)
 );
